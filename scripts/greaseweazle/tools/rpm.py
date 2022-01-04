9e81373f4e36a8ed52e76318af51646f85053b90
Keir Fraser <keir.xen@gmail.com>
2022-01-04 13:40:05 +0000

    gw rpm: Number of iterations configurable. Tweak output.

--- scripts/greaseweazle/tools/rpm.py
+++ scripts/greaseweazle/tools/rpm.py
@@ -7,23 +7,20 @@
 # This is free and unencumbered software released into the public domain.
 # See the file COPYING for more details, or visit <http://unlicense.org>.
 
-description = "Continuously measure RPM of drive spindle"
-
-import sys
-import importlib
+description = "Measure RPM of drive spindle."
 
 from greaseweazle.tools import util
-from greaseweazle import error
 from greaseweazle import usb as USB
-from greaseweazle.flux import Flux
 
 def print_rpm(usb, args):
     """Prints spindle RPM.
     """
 
-    # Measure drive RPM.
-    flux = usb.read_track(2)
-    print("%.2f RPM" % (flux.sample_freq/flux.index_list[2]*60.0), end='\r')
+    for _ in range(args.nr):
+        flux = usb.read_track(1)
+        time_per_rev = flux.index_list[-1] / flux.sample_freq
+        print("Rate: %.2f rpm ; Period: %.2f ms"
+              % (60 / time_per_rev, time_per_rev * 1e3))
 
 
 def main(argv):
@@ -32,21 +29,18 @@ def main(argv):
     parser.add_argument("--device", help="greaseweazle device name")
     parser.add_argument("--drive", type=util.drive_letter, default='A',
                         help="drive to read (A,B,0,1,2)")
+    parser.add_argument("--nr", type=int, default=1,
+                        help="number of iterations")
     parser.description = description
     parser.prog += ' ' + argv[1]
     args = parser.parse_args(argv[2:])
 
     try:
         usb = util.usb_open(args.device)
-
-        while True:
-            util.with_drive_selected(print_rpm, usb, args)
+        util.with_drive_selected(print_rpm, usb, args)
     except USB.CmdError as err:
         print("Command Failed: %s" % err)
 
-
-if __name__ == "__main__":
-    main(sys.argv)
 
 # Local variables:
 # python-indent: 4
