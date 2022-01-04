0f22bb8a2c79ec54d3366b1331017d2babe91960
Keir Fraser <keir.xen@gmail.com>
2022-01-04 13:24:51 +0000

    gw erase: Get rid of line overwrites.

--- scripts/greaseweazle/tools/erase.py
+++ scripts/greaseweazle/tools/erase.py
@@ -22,11 +22,9 @@ def erase(usb, args):
 
     for t in args.tracks:
         cyl, head = t.cyl, t.head
-        print("\rErasing Track %u.%u..." % (cyl, head), end="")
+        print("Erasing Track %u.%u" % (cyl, head))
         usb.seek(t.physical_cyl, t.physical_head)
         usb.erase_track(drive_ticks * 1.1)
-
-    print()
 
 
 def main(argv):
