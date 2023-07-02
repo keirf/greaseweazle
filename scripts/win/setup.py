7f42fd7e1d0da0cc4d0fe2fa685301a7ca03c5e6
Keir Fraser <keir.xen@gmail.com>
2023-07-02 10:46:05 +0100

    cx_Freeze: Install correct 32-bit DLLs from SysWOW64 on 32-bit build

--- scripts/win/setup.py
+++ scripts/win/setup.py
@@ -1,8 +1,19 @@
 from cx_Freeze import setup, Executable
 
+# Windows 32-bit build: We need to explicitly point at the 32-bit DLLs early.
+import sys, os
+if sys.maxsize <= 2**32:
+    new_paths = ['C:\\Windows\\SysWOW64\\downlevel']
+    old_paths = os.environ['PATH'].split(os.pathsep)
+    os.environ['PATH'] = os.pathsep.join(new_paths + old_paths)
+    bin_path_includes = new_paths
+else:
+    bin_path_includes = []
+
 buildOptions = dict(
     packages = ['greaseweazle'],
     excludes = ['tkinter', 'test', 'distutils'],
+    bin_path_includes = bin_path_includes,
     include_msvcr = True)
 
 base = 'Console'
