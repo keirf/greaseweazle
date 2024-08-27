90c2f9ab11ca5018a7cdcd2b0ae60ac398f66fbb
Francois Planque <github@fplanque.net>
2024-08-27 02:39:47 +0200

    doc + change default
    I believe 1s320 is the most commonly used format in the community.

--- src/greaseweazle/image/fd.py
+++ src/greaseweazle/image/fd.py
@@ -4,12 +4,17 @@
 #
 # This is free and unencumbered software released into the public domain.
 # See the file COPYING for more details, or visit <http://unlicense.org>.
+#
+# .fd is a widely used format in the Thomson MO/TO community.
+# .fd is directly supported by emulators such as DCMOTO.
+# The main difference between IMG and FD is that FD completely separates both sides of a disk,
+# i-e, all tacks/sectors of side 0 are stored before all tracks/sectors of side 1.
 
 from greaseweazle.image.img import IMG
 
 class FD(IMG):
-    default_format = 'thomson.1s80'
-    sequential = True
+    default_format = 'thomson.1s320'    # Most commonly used format
+    sequential = True                   # while side 0 before whole side 1
 
 # Local variables:
 # python-indent: 4
