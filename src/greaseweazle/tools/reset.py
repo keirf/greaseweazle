3d723b7c4299ee69cbda210527ce68af36dfd004
Keir Fraser <keir.xen@gmail.com>
2024-10-03 17:06:27 +0100

    gw reset: By default do not reset configured delay parameters

--- src/greaseweazle/tools/reset.py
+++ src/greaseweazle/tools/reset.py
@@ -12,19 +12,25 @@ description = "Reset the Greaseweazle device to power-on default state."
 import sys
 
 from greaseweazle.tools import util
+from greaseweazle.tools.delays import Delays
 from greaseweazle import usb as USB
 
 def main(argv) -> None:
 
     parser = util.ArgumentParser(usage='%(prog)s [options]')
     parser.add_argument("--device", help="device name (COM/serial port)")
+    parser.add_argument("--delays", action="store_true",
+                        help="include \"gw delays\" in the reset")
     parser.description = description
     parser.prog += ' ' + argv[1]
     args = parser.parse_args(argv[2:])
 
     try:
         usb = util.usb_open(args.device)
+        delays = Delays(usb)
         usb.power_on_reset()
+        if not args.delays:
+            delays.update()
     except USB.CmdError as error:
         print("Command Failed: %s" % error)
 
